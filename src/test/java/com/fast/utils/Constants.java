70eec5fecbcf3d37a71531d814e2c7c6c002cc66
olaruiulia <olaru.iulia93@gmail.com>
2019-02-18 23:39:27 +0200

    Constants

---
+++ src/test/java/com/fast/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.fast.utils;
+
+public class Constants {
+
+    public static final String USER_EMAIL = "olaru_iulia@yahoo.com";
+    public static final String USER_PASSWORD = "NewPassword!1993";
+    public static final String ADMIN_USERNAME = "admin";
+    public static final String ADMIN_PASSWORD = "parola11";
+}
